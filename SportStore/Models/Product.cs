b14ed2c71a70ec2a1b07ee1c7be11819b9ca6efd
duongnhatquang18@gmail.com <duongnhatquang18@gmai.com>
2020-03-03 16:08:47 +0700

    Add the key for product - Add the class DataContext

--- SportStore/Models/Product.cs
+++ SportStore/Models/Product.cs
@@ -7,6 +7,8 @@ namespace SportStore.Models
 {
     public class Product
     {
+        public long Id { get; set; }
+
         public string Name { get; set; }
         public string Category { get; set; }
         public decimal PurchasePrice { get; set; }
